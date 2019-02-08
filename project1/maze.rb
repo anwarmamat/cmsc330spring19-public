3c7827e72daabd123869613402d575b51ef4152c
jjubilee <jerrj33@gmail.com>
2019-02-08 12:36:04 -0500

    added open method

--- project1/maze.rb
+++ project1/maze.rb
@@ -1,18 +1,9 @@
-#!/usr/local/bin/ruby
-
 # ########################################
-# CMSC 330 - Project 1
+# CMSC330---SPRING2019---PROJECT1
+# Jeremy Ryan Jubilee
+# UID: 115265660
 # ########################################
-
-#-----------------------------------------------------------
-# FUNCTION DECLARATIONS
-#-----------------------------------------------------------
-
-def parse(file)
-  puts "Not yet implemented"    
-end
-
-#-----------------------------------------------------------
+#----------------------------------------------------------------------------
 # the following is a parser that reads in a simpler version
 # of the maze files.  Use it to get started writing the rest
 # of the assignment.  You can feel free to move or modify 
@@ -43,6 +34,44 @@ def read_and_print_simple_file(file)
     end
   end
 end
+#
+#
+#
+#
+#
+#----------------------------------
+# Begin my functions
+
+#----------------------------------
+# Function: open(file)
+# open() simply lists the number of cells wherein all directions are open.
+
+def openct(file)
+  
+  # do this to ignore first line (header)
+  line = file.gets
+  
+  # tracks no. of "open" cells
+  opencnt = 0
+
+  while line = file.gets do
+
+    # begins with "path", must be path specification
+    if line[0...4] != "path"
+
+    	# looks for udlr in any order, 4 in a row to indicate all directions.
+    	# since we assume file is valid, this will work 
+    	if line =~ /[udlr]{4}/
+      		opencnt += 1
+      	end
+    end
+  end
+
+  puts "#{opencnt}"
+end
+
+
+
 
 #----------------------------------
 def main(command_name, file_name)
@@ -54,8 +83,12 @@ def main(command_name, file_name)
     parse(maze_file)
   when "print"
     read_and_print_simple_file(maze_file)
+  when "open"
+  	openct(maze_file)
   else
     fail "Invalid command"
   end
 end
+
+
 
